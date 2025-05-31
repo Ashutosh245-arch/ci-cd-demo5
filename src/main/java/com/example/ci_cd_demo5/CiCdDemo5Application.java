1fa394deaf937e8f6024c2205bef83f0ebe13f85
Ashutosh Dhoka <dhokashutosh70@gmail.com>
2025-05-31 06:14:15 +0530

    Initial commit

---
+++ src/main/java/com/example/ci_cd_demo5/CiCdDemo5Application.java
@@ -0,0 +1,13 @@
+package com.example.ci_cd_demo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdDemo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ci_cd_demo5/CiCdDemo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ci_cd_demo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
